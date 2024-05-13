4089db74b589e2ddd588a7a1de415a050b2160c6
Batulzii Badraa <badraa@Badraas-MacBook-Air.local>
2024-05-13 13:08:02 +0800

    Added SecondPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import FirstPage from "./components/FirstPage";
+import SecondPage from "./components/SecondPage";
 
 export default function Home() {
   return (
     <div>
       <FirstPage/> 
+      <SecondPage/>
     </div>
     
   );
